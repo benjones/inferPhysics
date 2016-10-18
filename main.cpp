7e2d2d5c1f6d0c05c99bc3db3491958c3a16fb0d
Frank Haigh <Frankhaigh07@gmail.com>
2016-10-18 12:54:13 -0600

    Fixing errors with .head()

--- main.cpp
+++ main.cpp
@@ -83,7 +83,7 @@ Scalar computeEnergy(const MatrixXd& targets, const Matrix<Scalar, Eigen::Dynami
 	for (int i = 0; i < targets.cols(); i++) {
 	  //this approach is probably more efficient and (arguably) clearer
 	  // sum_i  exp(-i)|| what we have now||^2
-	  ret +=  exp(-i)*(guessI - targets.col(i).head(2).template cast<Scalar>()).squaredNorm();
+	  ret +=  exp(-i)*(guessI - targets.col(i).template cast<Scalar>()).squaredNorm();
 	  
 	  guessI = M*guessI;
 
