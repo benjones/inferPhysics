cc3fe294c80cea36ba989326e154d9b2baa4384c
Frank Haigh <Frankhaigh07@gmail.com>
2016-10-18 19:57:45 -0600

    Changed auto gradF = computeEnergy(X.topRows(2), convertToMatrixXd(M).block<2,2>(0,0));

--- main.cpp
+++ main.cpp
@@ -163,10 +163,8 @@ int main(){
 	  }
 	}
 	
-	std::cout << "2x2 block of M:\n" << convertToMatrixXd(M).block<2, 2>(0, 0) << std::endl;
-	std::cout << "vector: " << convertToMatrixXd(M).block<2, 2>(0, 0)*X.col(0).topRows(2) << std::endl;
-	std::cin.get();
-	auto energyAndDerivatives = computeEnergy(X,M);
+	
+	auto energyAndDerivatives = computeEnergy(X.topRows(2),convertToMatrixXd(M).block<2, 2>(0, 0));
 	gradNorm = 0;
 	for(auto r = 0; r < M.rows(); r++){
 	  for(auto c = 0; c < M.cols(); c++){
